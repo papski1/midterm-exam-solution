65c50d0d8ceb604ee775ba917f8e313bd15ddccb
unknown <YY439@ccs.local>
2025-03-14 19:09:48 +0800

    question 3 complete

---
+++ question3.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Express is working! Rey Mark Rivas'});
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
